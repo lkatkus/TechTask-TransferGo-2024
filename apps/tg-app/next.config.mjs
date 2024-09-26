b6cb9494cedf55416aa7e3a5f6a223ec6b9d37fd
Laimonas Katkus <laimonas@katkus.eu>
2024-09-26 19:48:13 +0300

    tg: fix basePath for github pages

--- apps/tg-app/next.config.mjs
+++ apps/tg-app/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/TechTask-TransferGo-2024/app",
   output: "export",
   transpilePackages: ["tg-ui"],
   distDir: "../../docs/app",
